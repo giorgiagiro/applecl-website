d06abcd57f6bb5124ad0f8830ecd3d08bca73a74
Giorgia <giorgia.giro@gmail.com>
2025-01-19 20:51:31 +0200

    features

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from "react"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+//import HowItWorks from './components/HowItWorks';
+//import Footer from './components/Footer';
 
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Model from "./components/Model"
+import * as Sentry from '@sentry/react';
 
 const App = () => {
-    return (
-        <>
-            <main className="bg-black">
-                <Navbar />
-                <Hero />
-                <Highlights />
-                <Model />
-            </main>
-        </>
-    )
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      {/*<HowItWorks />*/}
+      {/*<Footer />*/}
+    </main>
+  )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,29 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://607140c4cf1bb903d47cc0bc27e82547@o4508671436128256.ingest.de.sentry.io/4508671437963344",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    //Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
