54243acf967ff368e13a1198179cb42ea63c7f27
Giorgia <giorgia.giro@gmail.com>
2025-01-20 11:47:41 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Highlights from './components/Highlights';
 import Model from './components/Model';
 import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
-//import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 import * as Sentry from '@sentry/react';
 
@@ -17,7 +17,7 @@ const App = () => {
       <Model />
       <Features />
       <HowItWorks />
-      {/*<Footer />*/}
+      <Footer />
     </main>
   )
 }
